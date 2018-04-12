189ea59989867e7c5ccb5e00631f61ac5d518855
Yang Bo <yangbo12@huawei.com>
2018-04-12 16:32:14 +0800

    SCB-479 Add proxy for service center
    Service Center can now be accessed through :/sc/xxx. This will allow
    users to visit the frontend without running service center on public
    network.

--- frontend/main.go
+++ frontend/main.go
@@ -19,16 +19,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/url"
+
 	"github.com/apache/incubator-servicecomb-service-center/frontend/schema"
 	"github.com/astaxie/beego"
-	"log"
-	"net/http"
-	//"strconv"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	frontendIp := beego.AppConfig.String("frontend_host_ip")
 	frontendPort := beego.AppConfig.DefaultInt("frontend_host_port", 30103)
+
+	scIp := beego.AppConfig.DefaultString("httpadr", "127.0.0.1")
+	scPort := beego.AppConfig.DefaultInt("httpport", 30100)
 
 	// command line flags
 	port := flag.Int("port", frontendPort, "port to serve on")
@@ -36,17 +41,39 @@ func main() {
 
 	flag.Parse()
 
+	e := echo.New()
 	// handle all requests by serving a file of the same name
-	fs := http.Dir(*dir)
-	fileHandler := http.FileServer(fs)
-	http.Handle("/", fileHandler)
+	e.Static("/", *dir)
 
-	schemaHandler := schema.TestSchema()
-	http.Handle("/testSchema/", schemaHandler)
+	e.Any("/testSchema/", schema.SchemaHandleFunc)
 
+	// setup proxy for requests to service center
+	scAddr := fmt.Sprintf("http://%s:%d", scIp, scPort)
+	scUrl, err := url.Parse(scAddr)
+	log.Printf("sc addr:%s", scAddr)
+	if err != nil {
+		log.Fatalf("Error parsing service center address:%s, err:%s", scAddr, err)
+	}
+	targets := []*middleware.ProxyTarget{
+		{
+			URL: scUrl,
+		},
+	}
+	g := e.Group("/sc")
+	balancer := middleware.NewRoundRobinBalancer(targets)
+	pcfg := middleware.ProxyConfig{
+		Balancer: balancer,
+		Skipper:  middleware.DefaultSkipper,
+		Rewrite: map[string]string{
+			"/sc/*": "/$1",
+		},
+	}
+	g.Use(middleware.ProxyWithConfig(pcfg))
+
+	// run frontend web server
 	log.Printf("Running on port %d\n", *port)
-
 	addr := fmt.Sprintf("%s:%d", frontendIp, *port)
+
 	// this call blocks -- the progam runs here forever
-	fmt.Println(http.ListenAndServe(addr, nil))
+	log.Printf("Error: %s", e.Start(addr))
 }
